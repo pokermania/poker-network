f7d921356446712e5ea7cc121c90d44dfbdef66e
Hannes Uebelacker <hannes@pokermania.de>
2012-07-24 16:42:20 +0200

    poker-engine refactoring
     * cleaned up some unused/old files
     * using distutils to build poker-engine
     * no .in files in poker-engine (fixed tests regarding this)

--- poker-reflogging/setup.py
+++ poker-reflogging/setup.py
@@ -1,6 +1,14 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from distutils.core import setup
+sys.path.append('../common')
+from setup_extensions import TestCommand
+
 setup(
     name = 'reflogging',
     version = '1.0.0',
-    packages = ['reflogging']
+    packages = ['reflogging'],
+    cmdclass = {'test': TestCommand}
 )
