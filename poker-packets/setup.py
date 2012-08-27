d569b8331e90d3d11e926638a0a5553023f90d7c
Andor Goetzendorff <goetzend@cs.tum.edu>
2012-08-27 15:10:09 +0200

    changes in setup.py

--- poker-packets/setup.py
+++ poker-packets/setup.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 from distutils.core import setup
-sys.path.append('../common')
-from setup_ext.test import TestCommand
 
 setup(
     name = 'poker-packets',
     version = '2.2.0',
     packages = ['pokerpackets'],
-    cmdclass = {'test': TestCommand}
 )
